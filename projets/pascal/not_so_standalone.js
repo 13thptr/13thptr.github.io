ed52d05b218e26440a2feccc81b26d2090e381b8
Mehdi S <sahtali.mehdi@gmail.com>
2023-05-15 21:51:01 +0200

    modifications de formatage mineures

--- projets/pascal/not_so_standalone.js
+++ projets/pascal/not_so_standalone.js
@@ -2,26 +2,29 @@
             2019 Mehdi S.
             Algorithme différent: au lieu de calculer le coefficient binomial,
             on utilise le (corollaire du) théorème de Lucas:https://fr.wikipedia.org/wiki/Th%C3%A9or%C3%A8me_de_Lucas
-            Cet algorithme est très efficace puisqu'on ne perd pas de temps à calculer et stocker 
+            Cet algorithme est très efficace - en tout cas bien plus efficace que le précédent-
+            puisqu'on ne perd pas de temps à calculer et stocker 
             les coefficients de quelque manière que ce soit.
 */
-const c = document.getElementById("c");
-const ctx = c.getContext("2d");
-const btn = document.getElementById("btn");
-const LIGNES = c.width;
-const COLONNES = c.height;
-const COTE_PIXEL = 1;
+/*-------Interaction DOM-------*/
+const c = document.getElementById("c");//Interface de l'élément canevas
+const ctx = c.getContext("2d");//Contexte du canevas
 
+const btn = document.getElementById("btn");
 const mslider = document.getElementById("modulo");
 const p = document.getElementById("txt");
 
-let BASE = 2;
 let check1 = document.getElementById("oui");
 let check2 = document.getElementById("non");
 check1.checked = false;
+/*-----------------Constantes utiles-----------*/
 
-
-mslider.oninput = g = () => {
+const LIGNES = c.width;
+const COLONNES = c.height;
+const COTE_PIXEL = 1;
+let BASE = 2;//Pas vraiment une constante mais une variable globale dépendante de l'entrée utilisateur.
+//Fonction anonyme ne servant qu'à modifier la valeur de la base choisie.
+mslider.oninput = () => {
     BASE = mslider.value, p.innerHTML = "Triangle modulo " + BASE;
 }
 /*
@@ -38,14 +41,15 @@ let f = (u, v) => u.toString(v).split``.map(e => Number(e));
     que le chiffre correspondant de m. 
     Ce corollaire est un cas particulier d'un théorème de Kummer.» 
 
-    (Renvoie "vrai"(true) si le coefficient binomial fourni par les arguments colonne et ligne est divisible
-    par p,"faux" (false)sinon)
+    (
+    Renvoie "vrai"(true) si le coefficient binomial fourni par les arguments colonne et ligne est divisible
+    par p,"faux" (false)sinon (ou l'autre mais il y a bien une séparation  au bon moment, c'est ce qui compte graphiquement)
+    )
 */
 
 function moduloLucas(ligne, colonne, p) {
-
     if (colonne > ligne) {
-        return true;
+        return false;
     }
     //On développe m et n en base p
     let m = f(ligne, p);
@@ -53,6 +57,7 @@ function moduloLucas(ligne, colonne, p) {
     return compare(m, n);
 
 }
+//let moduloLucas=(l,c,p)=>(c>l)&&(compare f(l,p),f(c,p))
 //Si un chiffre de tab2 est supérieur au chiffre de tab1 correspondant,on renvoie vrai, et faux sinon.
 function compare(tab1, tab2) {
 
@@ -71,10 +76,10 @@ function compare(tab1, tab2) {
 
     for (let k = 0; k < tab1.length; k++) {
         if (tab2[k] > tab1[k]) {
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 
@@ -90,7 +95,7 @@ function Generer() {
     }
     for (X = 0; X < COLONNES; X++) {
         for (Y = 0; Y < LIGNES; Y++) {
-            if (!moduloLucas(Y, X, BASE)) {
+            if (moduloLucas(Y, X, BASE)) {
                 ctx.fillRect(X * COTE_PIXEL - Y * COTE_PIXEL / 2 * FLAG + COLONNES / 2 * FLAG, Y * COTE_PIXEL, COTE_PIXEL, COTE_PIXEL);
             }
         }
